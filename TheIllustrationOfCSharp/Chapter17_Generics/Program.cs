3f0e357bfafbb01a8163253c2f1b75660bce503f
DuanBinbin <binbin.duan@pisofttech.com>
2018-08-24 12:24:46 +0800

    添加泛型

---
+++ TheIllustrationOfCSharp/Chapter17_Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter17_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public abstract class CBase
+        {
+            public abstract void Show();
+        }
+
+        public class C<T> : CBase
+        {
+            public override void Show()
+            {
+                                
+            }
+        }        
+    }
+}
